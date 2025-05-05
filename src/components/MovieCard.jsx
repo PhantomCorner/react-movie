937bfc599d09e34f5005bff0e7a96cc30de10020
PhantomCorner <jiaojiaobigbang@gmail.com>
2025-05-05 20:02:05 +1200

    init

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,22 @@
+function MovieCard({ Movie }) {
+  function favoriteOnclick() {
+    console.log("clicked");
+  }
+  return (
+    <div className="movie-card">
+      <div className="mover-poster">
+        <img src={Movie.url} alt={Movie.title} />
+        <div className="movie-overlay">
+          <button className="favorite-btn" onClick={favoriteOnclick}>
+            Like
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{Movie.title}</h3>
+        <p>{Movie.release_date}</p>
+      </div>
+    </div>
+  );
+}
+export default MovieCard;
