0be0f4a2abe9db4c89268dd4afa0204c168a50dd
PhantomCorner <jiaojiaobigbang@gmail.com>
2025-05-05 22:46:45 +1200

    update favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "../css/Favorites.css";
+import { useMovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
+
 export default function Favorites() {
-  return (
-    <div className="favorites-empty">
-      <h2>No Favorites yet</h2>
-      <p>Add some movies now</p>
-    </div>
-  );
+  const { favorites } = useMovieContext();
+  if (favorites) {
+    return (
+      <div className="favorites">
+        <h2>Your Favorites</h2>
+        <div className="movies-grid">
+          {favorites.map((item) => (
+            <MovieCard Movie={item} key={item.id} />
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="favorites-empty">
+        <h2>No Favorites yet</h2>
+        <p>Add some movies now</p>
+      </div>
+    );
+  }
 }
