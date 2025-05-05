fbc398e06962072cc14187020247ee3d2518bde4
PhantomCorner <jiaojiaobigbang@gmail.com>
2025-05-05 22:35:04 +1200

    check movie id in favorites

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -6,6 +6,26 @@ export const MovieProvider = ({ children }) => {
   useEffect(() => {
     const storedFavs = localStorage.getItem("favorites");
     if (storedFavs) setFavorites(JSON.parse(storedFavs));
-  });
-  return <MovieContext.Provider>{children}</MovieContext.Provider>;
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+  const addToFavorites = (movie) => {
+    setFavorites((prev) => [...prev, movie]);
+  };
+  const removeFromFavorites = (movieID) => {
+    setFavorites((prev) => prev.filter((movie) => movie.id !== movieID));
+  };
+  const isFavorite = (movieID) => {
+    return favorites.some((movie) => movie.id === movieID);
+  };
+  const value = {
+    favorites,
+    addToFavorites,
+    removeFromFavorites,
+    isFavorite,
+  };
+  return (
+    <MovieContext.Provider value={value}>{children}</MovieContext.Provider>
+  );
 };
